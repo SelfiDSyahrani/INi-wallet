b86c00742046242ae2ec562e6336fdf87bdb6df8
adiyuda <adiyudapratama4@gmail.com>
2023-02-27 16:39:12 +0700

    modifield model transaction init payment method and status

--- model/transaction.go
+++ model/transaction.go
@@ -7,6 +7,8 @@ type Transaction struct {
 	User_ID             string    `json:"user_ID"`
 	Money_Changer_ID    string    `json:"money_Changer_ID"`
 	Transaction_Type_ID string    `json:"transaction_Type_ID"`
+	Payment_method_id   string    `json:"payment_method_id"`
 	Amount              float64   `json:"amount"`
+	Status              bool      `json:"status"`
 	Date_Time           time.Time `json:"date_Time"`
 }
