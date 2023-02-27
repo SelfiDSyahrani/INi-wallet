70dbb88e8dcdea7190f7104e570f79f11b616a32
adiyuda <adiyudapratama4@gmail.com>
2023-02-27 16:54:24 +0700

    modified transaction repository

--- repository/transaction_repository.go
+++ repository/transaction_repository.go
@@ -2,41 +2,61 @@ package repository
 
 import (
 	"INi-Wallet/model"
-
-	"gorm.io/gorm"
+	"INi-Wallet/utils"
+	"github.com/jmoiron/sqlx"
+	//"gorm.io/gorm"
 )
 
 type TransactionRepository interface {
-	Create(transaction model.Transaction) error
 	GetByID(transaction_ID string) (model.Transaction, error)
 	GetAll() ([]model.Transaction, error)
+	Delete(transaction_ID string) error
 }
 
 type transactionRepository struct {
-	db *gorm.DB
+	db *sqlx.DB
 }
 
-//create a new transaction
-func (tr *transactionRepository) Create(transaction model.Transaction) error {
-	return tr.db.Create(transaction).Error
+//GetByID
+func (tr *transactionRepository) GetByID(transaction_ID string) (model.Transaction, error) {
+	var transaction model.Transaction
+	err := tr.db.QueryRow(utils.SELECT_TRANSACTION_ID, transaction_ID).Scan(
+		&transaction.Transaction_ID,
+		&transaction.User_ID,
+		&transaction.Money_Changer_ID,
+		&transaction.Transaction_Type_ID,
+		&transaction.Payment_method_id,
+		&transaction.Amount,
+		&transaction.Status,
+		&transaction.Date_Time,
+	)
+	if err != nil {
+		return model.Transaction{}, err
+	}
+	return transaction, nil
 }
 
-//GetByID retrieves a user by ID
-func (tr * transactionRepository) GetByID(transaction_ID string) (model.Transaction, error) {
-	 transaction := model.Transaction{}
-	 err := tr.db.First(transaction,transaction_ID).Error
-	 return transaction,err
-}
-
-// GetAll retrieves all transactions
-func (tr *transactionRepository)GetAll() ([]model.Transaction, error)  {
+// GetAll
+func (tr *transactionRepository) GetAll() ([]model.Transaction, error) {
 	var transactions []model.Transaction
-	err :=tr.db.Find(&transactions).Error
-	return transactions,err
+	err := tr.db.Select(transactions, utils.SELECT_TRANSACTION)
+	if err != nil {
+		return nil, err
+	}
+	return transactions, nil
 }
 
-//object 
-func NewTransactionRepository(db * gorm.DB) TransactionRepository {
+// delete
+func (tr *transactionRepository) Delete(transaction_ID string) error {
+	_, err := tr.db.Exec(utils.DELETE_TRANSACTION, transaction_ID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+//object
+func NewTransactionRepository(db *sqlx.DB) TransactionRepository {
 	return &transactionRepository{
 		db: db,
 	}
