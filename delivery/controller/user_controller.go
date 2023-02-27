35292413c508474df4771a64a0199bb09c918080
selfi <selfidwis58@gmail.com>
2023-02-27 09:01:47 +0700

    add config and delivery/controller

---
+++ delivery/controller/user_controller.go
@@ -0,0 +1,57 @@
+package controller
+
+import (
+	"INi-Wallet/model"
+	"INi-Wallet/usecase"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type UserController struct {
+	router  *gin.Engine
+	usecase usecase.UserUseCase
+}
+
+func (cc *UserController) registerCustomer(ctx *gin.Context) {
+	var user model.User
+	if err := ctx.ShouldBindJSON(&user); err != nil {
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"err": err.Error()})
+		return
+	}
+	if err := cc.usecase.RegisterUser(user); err != nil {
+		ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"err": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, user)
+}
+
+func (cc *UserController) getAllUser(ctx *gin.Context) {
+	users, err := cc.usecase.GetAllUsers()
+	if err != nil {
+		ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"err": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, users)
+}
+
+func (cc *UserController) getUserById(ctx *gin.Context) {
+	id := ctx.Param("id")
+	user, err := cc.usecase.GetUserByID(id)
+	if err != nil {
+		ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"err": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, user)
+}
+
+func NewCustomerController(r *gin.Engine, usecase usecase.UserUseCase) *UserController {
+	controller := UserController{
+		router:  r,
+		usecase: usecase,
+	}
+	r.GET("/customer", controller.getAllUser)
+	r.GET("/customer/:id", controller.getUserById)
+	r.POST("/customer", controller.registerCustomer)
+	return &controller
+}
