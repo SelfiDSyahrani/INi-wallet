775d6e0a6b6629b21bc2671318ef03cae3f17914
adiyuda <adiyudapratama4@gmail.com>
2023-02-27 11:41:00 +0700

    modified transaction_repository

--- repository/transaction_repository.go
+++ repository/transaction_repository.go
@@ -21,14 +21,23 @@ func (tr *transactionRepository) Create(transaction model.Transaction) error {
 	return tr.db.Create(transaction).Error
 }
 
+//GetByID retrieves a user by ID
 func (tr * transactionRepository) GetByID(transaction_ID string) (model.Transaction, error) {
 	 transaction := model.Transaction{}
 	 err := tr.db.First(transaction,transaction_ID).Error
 	 return transaction,err
 }
 
+// GetAll retrieves all transactions
 func (tr *transactionRepository)GetAll() ([]model.Transaction, error)  {
 	var transactions []model.Transaction
 	err :=tr.db.Find(&transactions).Error
 	return transactions,err
+}
+
+//object 
+func NewTransactionRepository(db * gorm.DB) TransactionRepository {
+	return &transactionRepository{
+		db: db,
+	}
 }
