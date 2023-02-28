0ced1f7bf617c54433a82388886ad5e21fedc71d
adiyuda <adiyudapratama4@gmail.com>
2023-02-28 11:08:43 +0700

    .

--- utils/raw_query.go
+++ utils/raw_query.go
@@ -8,10 +8,9 @@ const(
 	//UPDATE_PAYMENT_METHOD = "UPDATE payment_method set payment_method_id=$1,method_name =$2 where id=$3"
 	DELETE_PAYMENT_METHOD = "DELETE FROM payment_method where payment_method_id = $1"
 
-<<<<<<< HEAD
 	//transfer
 
-	//
+	//user
 	INSERT_USER = "INSERT INTO user (userWallet_ID, name, email, phone, password, balance) values ($!,$2, $3, $4, $5, $6)"
 	DELETE_USER = "DELETE FROM user where userWallet_ID = $1"
 	SELECT_USER_ID = "SELECT * FROM user where userWallet_ID = $1"
@@ -20,13 +19,11 @@ const(
 	UPDATE_USER_PASS= "UPDATE user set set password where id=$1 AND email=$2"
 
 
-
-
-)
-=======
-	//transaction
 	SELECT_TRANSACTION = "SELECT * FROM transactions"
 	SELECT_TRANSACTION_ID = "SELECT * FROM transactions where transaction_id = $1"
 	DELETE_TRANSACTION = "DELETE FROM transactions where transaction_id = $1"
+
 )
->>>>>>> 70dbb88e8dcdea7190f7104e570f79f11b616a32
+	//transaction
+
+
