d4268f509e0b2ae05ce5280ae28ad02c2f0901ff
selfi <selfidwis58@gmail.com>
2023-02-28 10:24:29 +0700

    commit before merge

--- repository/user_repository.go
+++ repository/user_repository.go
@@ -1,34 +1,12 @@
 package repository
 
-import (
-<<<<<<< HEAD
-	"dev_selfi/model"
-	"dev_selfi/utils"
-=======
-	"INi-Wallet/model"
-	"INi-Wallet/utils"
->>>>>>> development
-
-	"github.com/jmoiron/sqlx"
-)
-
 type UserRepository interface {
-<<<<<<< HEAD
-=======
-	Insert(user *model.User) error
-	UpdateUserByID(id string) error
->>>>>>> development
 	Delete(userWallet_ID string) error
 	FindByEmail(email string) (*model.User, error)
 	GetByID(userWallet_ID string) (model.User, error)
 	GetAll() ([]model.User, error)
-<<<<<<< HEAD
 	Insert(user *model.User) (*model.User, error)
 	UpdateById(user model.User) error
-	
-=======
-	UpdateUserPass(id string, email string) error
->>>>>>> development
 }
 
 type userRepository struct {
@@ -36,14 +14,9 @@ type userRepository struct {
 }
 
 // Creates a new user
-<<<<<<< HEAD
 func (r *userRepository) Insert(user *model.User) (*model.User, error) {
 	query := "INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING id"
 	err := r.db.QueryRow(query, user.Name, user.Email, user.Password).Scan(&user.UserWallet_ID)
-=======
-func (r *userRepository) Insert(newuser *model.User) error {
-	_, err := r.db.NamedExec(utils.INSERT_USER, newuser)
->>>>>>> development
 	if err != nil {
 		return user, err
 	}
@@ -52,7 +25,7 @@ func (r *userRepository) Insert(newuser *model.User) error {
 
 // DeleteUser deletes an existing user
 func (r *userRepository) Delete(user_ID string) error {
-	_, err := r.db.Exec(utils.DELETE_USER, user_ID)
+	_, err := r.db.Exec(utils.DELETE_PAYMENT_METHOD, user_ID)
 	if err != nil {
 		return err
 	}
@@ -83,37 +56,21 @@ func (r *userRepository) GetAll() ([]model.User, error) {
 	return users, nil
 }
 
-<<<<<<< HEAD
 func (r *userRepository) UpdateById(user model.User) error {
-	_,err := r.db.NamedExec(utils.UPDATE_USER_BY_ID,user)
-=======
-// UpdateUser updates an existing user
-func (r *userRepository) UpdateUserByID(Id string) error {
-	_, err := r.db.Exec(utils.UPDATE_USER_BY_ID, Id)
->>>>>>> development
+	_, err := r.db.NamedExec(utils.UPDATE_USER_BY_ID, user)
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-<<<<<<< HEAD
 func (r *userRepository) FindByEmail(email string) (*model.User, error) {
 	users := model.User{}
-	err := r.db.Select(users, "SELECT * FROM users WHERE Email=$1", email)
+	err := r.db.Select(users, "SELECT * FROM user WHERE email=$1", email)
 	if err != nil {
 		return nil, err
 	}
 	return &users, err
-=======
-// UpdateUser updates an existing user
-func (r *userRepository) UpdateUserPass(Id string, email string) error {
-	_, err := r.db.Exec(utils.UPDATE_USER_PASS, Id, email)
-	if err != nil {
-		return err
-	}
-	return nil
->>>>>>> development
 }
 
 func NewUserRepository(db *sqlx.DB) UserRepository {
